c28b344a1a26ee82bdfcdd2e2adc1da59bf9ebc6
Jonathan <40722881+jonlarson22@users.noreply.github.com>
2026-07-08 21:33:08 -0600

    Initialize Firebase app and Firestore database

---
+++ firebase-init.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDj3_AxBIvy8Tg-M2AfzOSSDbSU0XdwWAU",
+  authDomain: "headsup-919b2.firebaseapp.com",
+  projectId: "headsup-919b2",
+  storageBucket: "headsup-919b2.appspot.com",
+  messagingSenderId: "94397181934",
+  appId: "1:94397181934:web:cd4426f3d92f482c3828ff"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
